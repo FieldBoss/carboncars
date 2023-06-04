6572076198390c8edcbcb594de702e0c497db2c4
FieldBoss <111802200+FieldBoss@users.noreply.github.com>
2023-06-04 11:58:01 -0600

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Mapper from '../Mapper/Mapper.js';
+import "./App.css";
+
+function App() {
+    return (
+        <> 
+        <Mapper />  
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Mapper/Mapper.js
@@ -0,0 +1,11 @@
+import InspectionForm from '../InspectionForm/InspectionForm';
+import Navbar from '../Navbar/Navbar';
+
+const Mapper = () => {  
+    return (
+        <>
+        <InspectionForm /> 
+        </>
+    ); 
+}
+export default Mapper;
